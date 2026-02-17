7bfe19215000de6485581691cf3d9e764b182108
Christophe Eymard <ceymard@pm.me>
2026-02-17 16:05:53 +0100

    somewhat working config right now

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My reproducible user environment with nixGL (Intel + NVIDIA)";
+  description = "My reproducible user environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -20,7 +20,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixgl, dank-material-shell, ... }:
+  outputs = { self, nixpkgs, home-manager, dank-material-shell, nixgl, ... }:
     let
       system = "x86_64-linux";
 
@@ -29,10 +29,10 @@
         config.allowUnfree = true;
       };
 
-      # nixGL derivations (launchers)
       nixGLPkgs = nixgl.packages.${system};
 
       username = builtins.getEnv "USER";
+
     in {
       homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -43,13 +43,6 @@
             home.homeDirectory = "/home/" + username;
             home.stateVersion = "26.05";
 
-            
-# Create a symlink so systemd can find the units
-  home.activation.linkSystemdUnits = home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD mkdir -p $HOME/.config/systemd/user
-    $DRY_RUN_CMD ln -sf $HOME/.nix-profile/lib/systemd/user/* $HOME/.config/systemd/user/
-  '';
-
             home.packages = with pkgs; [
               git
               tmux
@@ -59,23 +52,17 @@
               wget
               openssh # we want something modern
 
+              epiphany # debug webkit
+              ungoogled-chromium
+              go rustc cargo
+              goodvibes
+
               chezmoi helix
               fish
+              meson cmake
 
-              alacritty
-
-              # graphical stuff
-              niri xdg-desktop-portal-gnome 
-              
-              # nixGL launchers (NOT functions)
               nixGLPkgs.nixGLIntel
-              # nixGLPkgs.nixGLNvidia
               nixGLPkgs.nixVulkanIntel
-              # nixGLPkgs.nixVulkanNvidia
-
-              # Add DankMaterialShell package
-              dank-material-shell.packages.${system}.default
-              dank-material-shell.packages.${system}.quickshell
             ];
 
             programs.zsh.enable = true;
